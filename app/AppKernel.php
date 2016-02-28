d802b3a7cfc2b3b2c162b600e58a3defa6c361d1
David Marland <davidm@rland.me.uk>
2016-02-28 00:26:20 +0000

    Alpha env needs to load tools

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle()
         );
 
-        if (in_array($this->getEnvironment(), array('dev'))) {
+        if (in_array($this->getEnvironment(), array('dev','alpha'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
         }
