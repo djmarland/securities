d4818a93ae324de4ecfc2b9b5104fb56458df9b5
David Marland <davidm@rland.me.uk>
2016-03-29 21:59:11 +0100

    Alter Migration

--- src/SecuritiesService/Data/Database/Migration/Version20160329215745.php
+++ src/SecuritiesService/Data/Database/Migration/Version20160329215745.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160328140658 extends AbstractMigration
+class Version20160329215745 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,11 +18,9 @@ class Version20160328140658 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE config (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid_binary)\', site_title VARCHAR(255) NOT NULL, site_tagline VARCHAR(255) NOT NULL, features LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', uuid VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-    
+        $this->addSql('CREATE TABLE config (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid_binary)\', settings LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', features LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', uuid VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         // insert empty row
         $this->addSql('INSERT INTO config (created_at) VALUES (NOW())');
-    
     }
 
     /**
