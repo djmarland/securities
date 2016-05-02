fd3ac219292a1bfd17c11471610e75def17158b6
David Marland <davidm@rland.me.uk>
2016-05-02 14:03:20 +0100

    Finder and table collapse

--- src/AppBundle/Resources/views/Presenter/Organism/Finder/FinderItemPresenter.php
+++ src/AppBundle/Resources/views/Presenter/Organism/Finder/FinderItemPresenter.php
@@ -60,4 +60,26 @@ class FinderItemPresenter extends Presenter
     {
         return ($this->isActive()) ? 'finder__active' : '';
     }
+
+    public function getIndicatorClass(): string
+    {
+        return (!!$this->listName) ? '' : 'finder__indicator--nodrop';
+    }
+
+    public function getIcon(): string
+    {
+        if (!!$this->listName) {
+            return <<<SVG
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/><path fill="none" d="M0-.25h24v24H0z"/>
+</svg>
+SVG;
+
+        }
+        return <<<SVG
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="8" />
+</svg>
+SVG;
+    }
 }
